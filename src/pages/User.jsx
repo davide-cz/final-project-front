1d4ebd9231f9bd14ee43e8e3cfb8d8df2ed62e28
Davide Carletti <carletti.dav@gmail.com>
2024-02-20 12:09:42 +0100

    commit | fix vite uri

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -5,7 +5,6 @@ import MusiciansForm from "../modals/MusiciansForm";
 import { axiosOpts } from "../Ut/axiosOpt";
 import { Link } from "react-router-dom";
 
-const {VITE_VERCEL_URI}=import.meta.env
 const {VITE_URI}=import.meta.env
 
 export default function (){
@@ -21,20 +20,20 @@ export default function (){
     //patch su ruolo utente, per pubblicare annuncio devi essere un musicista
     
     const becomeMusician=()=>{
-        axios.patch(`${VITE_URI || VITE_VERCEL_URI}/user/${user.user_name}`,{...user,role:'musician'} , axiosOpts(token))
+        axios.patch(`${VITE_URI}/user/${user.user_name}`,{...user,role:'musician'} , axiosOpts(token))
         .then(obj=>{console.log('role Changed')})
         .catch(error=>console.error(error))
     }
 
     useEffect(()=>{
-        axios.get(`${VITE_VITE_URI || VITE_VERCEL_URIURI}/user/${user.user_name}`, axiosOpts(token))
+        axios.get(`${VITE_URI}/user/${user.user_name}`, axiosOpts(token))
         .then(res=>{console.log(`connected as ${user.user_name}`)})
     },[becomeMusician])
 
 //chiamata get che recupera tutti i musicisti con id dell'user ATTIVO
 
     useEffect(()=>{
-        axios.get(`${VITE_URI || VITE_VERCEL_URI}/musicians`, axiosOpts(token))
+        axios.get(`${VITE_URI}/musicians`, axiosOpts(token))
         .then(res=>{setMusiciansArray(res.data)
             setFilteredArray(res.data
                 .filter(mus=>mus.user?._id.includes(`${user?._id}`)))
